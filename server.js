dede71ffb1bf473de10a6e1039e885471693e2d1
terz99 <dusanterzic87@gmail.com>
2017-08-22 01:28:39 +0200

    lesson 15

--- server.js
+++ server.js
@@ -1,25 +1,46 @@
-// Lesson 13 and 14
+// // Lesson 13 and 14
+//
+// var http = require("http");
+// var fs = require("fs");
+//
+// // 404 response
+// function send404Response(response) {
+//     response.writeHead(404, {"Content-Type": "text/plain"});
+//     response.write("Error 404: Page not found!");
+//     response.end();
+// }
+//
+// // Handle a user request
+// function onRequest(request, response) {
+//
+//     if (request.method == "GET" && (request.url == "/" || request.url == "/index.html")) {
+//         response.writeHead(200, {"Content-Type": "text/html"});
+//         fs.createReadStream("./index.html").pipe(response);
+//     } else {
+//         send404Response(response);
+//     }
+// }
+//
+// http.createServer(onRequest).listen(8888);
+// console.log("Server is now running...");
 
+// Lesson 15
+
+var connect = require("connect");
 var http = require("http");
-var fs = require("fs");
 
-// 404 response
-function send404Response(response) {
-    response.writeHead(404, {"Content-Type": "text/plain"});
-    response.write("Error 404: Page not found!");
-    response.end();
+var app = connect();
+
+function profile(request, response) {
+    console.log("User requested profile");
 }
 
-// Handle a user request
-function onRequest(request, response) {
-
-    if (request.method == "GET" && (request.url == "/" || request.url == "/index.html")) {
-        response.writeHead(200, {"Content-Type": "text/html"});
-        fs.createReadStream("./index.html").pipe(response);
-    } else {
-        send404Response(response);
-    }
+function forum(request, response) {
+    console.log("User requested forum");
 }
 
-http.createServer(onRequest).listen(8888);
+app.use("/profile", profile);
+app.use("/forum", forum);
+
+http.createServer(app).listen(8888);
 console.log("Server is now running...");
