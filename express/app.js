c189f3f8202b2b2a9bd2d85505184128693b4283
terz99 <dusanterzic87@gmail.com>
2017-08-22 15:29:54 +0200

    lesson 20

--- express/app.js
+++ express/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var about = require("./routes/about");
 
 var app = express();
 
@@ -19,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use("/about", about);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
