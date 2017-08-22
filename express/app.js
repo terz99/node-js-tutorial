6563d9fa4a15a82e0de653cee190766f743a9ee4
terz99 <dusanterzic87@gmail.com>
2017-08-22 16:06:34 +0200

    lesson 22

--- express/app.js
+++ express/app.js
@@ -9,8 +9,6 @@ var about = require("./routes/about");
 
 var app = express();
 
-app.locals.videodata = require("./videodata");
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
--- express/routes/index.js
+++ express/routes/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var vd = require("../videodata");
+var path = require("path");
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'The New Boston',
-                        name: "Bucky"});
+    res.render('index', { title: 'The New Boston',
+    name: "Bucky",
+    videodata: vd,
+    path: path});
 });
 
 module.exports = router;
