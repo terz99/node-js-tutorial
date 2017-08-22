a0da8d31b1c72b261f5fe354027ed4832cc364df
terz99 <dusanterzic87@gmail.com>
2017-08-22 15:47:26 +0200

    lesson 21

--- express/app.js
+++ express/app.js
@@ -9,6 +9,8 @@ var about = require("./routes/about");
 
 var app = express();
 
+app.locals.videodata = require("./videodata");
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
